d5e723741e1885e8932c3ff12470fa5eac66c04f
Godfrey Jonas <jonasgodfrey@users.noreply.github.com>
2021-01-24 17:23:17 +0100

    my commit

--- breketebackend/resources/views/layouts/footer.blade.php
+++
@@ -1,8 +0,0 @@
-  <!-- Main Footer -->
-    <footer class="main-footer">
-        <div class="float-right d-none d-sm-block">
-            <b>Version</b> 1.0
-        </div>
-        <strong>Copyright &copy; 2014-2020 <a href="http://houseofamaya.com.ng/"> Powered By House of Amaya.</a>.</strong> All rights
-        reserved.
-    </footer>
